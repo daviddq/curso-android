3cfb96b3b6fe75f8f7b7c0542aafd0f093aa4041
David Diaz <daviddq@gmail.com>
2013-12-07 20:33:47 +0100

    Ya se muestra el mapa

--- src/es/daviddiaz/cursoandroid/tarea/activities/MainActivity.java
+++ src/es/daviddiaz/cursoandroid/tarea/activities/MainActivity.java
@@ -1,7 +1,5 @@
 package es.daviddiaz.cursoandroid.tarea.activities;
 
-import com.android.volley.RequestQueue;
-
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -18,7 +16,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import es.daviddiaz.cursoandroid.tarea.R;
 import es.daviddiaz.cursoandroid.tarea.dao.CentroComercialDao;
-import es.daviddiaz.cursoandroid.tarea.dao.FotoAdapter;
 import es.daviddiaz.cursoandroid.tarea.dao.FotosDao;
 import es.daviddiaz.cursoandroid.tarea.fragments.ComunidadFragment;
 import es.daviddiaz.cursoandroid.tarea.fragments.ImagenesFragment;
--- src/es/daviddiaz/cursoandroid/tarea/fragments/MapaFragment.java
+++ src/es/daviddiaz/cursoandroid/tarea/fragments/MapaFragment.java
@@ -1,16 +1,38 @@
 package es.daviddiaz.cursoandroid.tarea.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import es.daviddiaz.cursoandroid.tarea.R;
 
-public class MapaFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapaFragment extends SupportMapFragment {
+  private GoogleMap map=null;
+  private Bundle savedInstanceState;
+  public static final LatLng ALICANTE= new LatLng(38.20, 0.29);
+  
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_mapa, container, false);
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.savedInstanceState = savedInstanceState;
+  }
+  
+  @Override
+  public void onResume() {
+    super.onResume();
+    crearMapa();
+  }
+  
+  private void crearMapa() {
+    if (null==map) {
+      map = getMap();
+      if (null!=map) {
+        map.getUiSettings().setZoomControlsEnabled(false);
+        if (null==savedInstanceState) {
+          map.moveCamera(CameraUpdateFactory.newLatLngZoom(ALICANTE, 10));
+        }
+      }
+    }
   }
 }
