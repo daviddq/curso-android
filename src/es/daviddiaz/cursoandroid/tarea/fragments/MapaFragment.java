f7054b637600e163d9c3aaaa9981841427ff5ff1
David Diaz <daviddq@gmail.com>
2013-12-08 10:11:28 +0100

    refactor

--- src/es/daviddiaz/cursoandroid/tarea/fragments/MapaFragment.java
+++ src/es/daviddiaz/cursoandroid/tarea/fragments/MapaFragment.java
@@ -1,20 +1,36 @@
 package es.daviddiaz.cursoandroid.tarea.fragments;
 
+import android.app.Dialog;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MapaFragment 
 extends 
   SupportMapFragment
+implements 
+  ConnectionCallbacks,
+  OnConnectionFailedListener
 {
   private GoogleMap map=null;
   private Bundle savedInstanceState;
-  public static final LatLng ALICANTE= new LatLng(38.34600, -0.49069);
+  
+  public static final int MILLISECONDS_PER_SECOND = 1000;
+  public static final long UPDATE_INTERVAL_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * 5;
+  public static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * 1; 
+  public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+  LocationClient locationClient;
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -23,9 +39,63 @@ extends
   }
   
   @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    
+    locationClient = new LocationClient(getActivity(), this, this);
+  }
+  
+  @Override
   public void onResume() {
     super.onResume();
     crearMapa();
+  }
+
+  @Override
+  public void onConnectionFailed(ConnectionResult connectionResult) {
+    if (connectionResult.hasResolution()) {
+      try {
+        connectionResult.startResolutionForResult(
+            getActivity(),
+            CONNECTION_FAILURE_RESOLUTION_REQUEST);
+      } catch (IntentSender.SendIntentException e) {
+        Log.e("ERROR",Log.getStackTraceString(e));
+      }
+    } else {
+      Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+          connectionResult.getErrorCode(),
+          getActivity(),
+          CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+      if (errorDialog != null) {
+        ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+        errorFragment.setDialog(errorDialog);
+        errorFragment.show(getActivity().getSupportFragmentManager(), "dialog");
+      }
+    }
+  }
+  
+  @Override
+  public void onStart() {
+    super.onStart();
+    locationClient.connect();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    if (locationClient.isConnected()) {
+      locationClient.disconnect();
+    }
+  }
+
+  @Override
+  public void onConnected(Bundle connectionHint) {
+    actualizarPosicion(locationClient.getLastLocation());
+  }
+
+  @Override
+  public void onDisconnected() {
   }
   
   private void crearMapa() {
@@ -34,10 +104,17 @@ extends
       if (null!=map) {
         map.getUiSettings().setZoomControlsEnabled(false);
         if (null==savedInstanceState) {
-          map.moveCamera(CameraUpdateFactory.newLatLngZoom(ALICANTE, 10));
           map.setMyLocationEnabled(true);
         }
       }
+    }
+  }
+
+  private void actualizarPosicion(Location posicion) {
+    if (posicion != null) {
+      LatLng coordenadas = new LatLng(posicion.getLatitude(),
+        posicion.getLongitude());
+      map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenadas, 10));
     }
   }
 }
--- src/es/daviddiaz/cursoandroid/tarea/fragments/TiendasFragment.java
+++ src/es/daviddiaz/cursoandroid/tarea/fragments/TiendasFragment.java
@@ -1,8 +1,6 @@
 package es.daviddiaz.cursoandroid.tarea.fragments;
 
 import android.app.Dialog;
-import android.content.IntentSender;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,18 +8,12 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBar.TabListener;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
-import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.LocationClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 
 import es.daviddiaz.cursoandroid.tarea.R;
 import es.daviddiaz.cursoandroid.tarea.activities.MainActivity;
@@ -30,10 +22,7 @@ public class TiendasFragment
 extends 
   Fragment
 implements 
-  TabListener,
-  ConnectionCallbacks,
-  OnConnectionFailedListener,
-  LocationListener
+  TabListener
 {
   private Fragment[] fragments = {
     new ListadoTiendasFragment(),
@@ -42,13 +31,6 @@ implements
 
   static final int LISTADO = 0;
   static final int MAPA = 1;
-
-  public static final int MILLISECONDS_PER_SECOND = 1000;
-  public static final long UPDATE_INTERVAL_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * 5;
-  public static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * 1; 
-  public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-  LocationClient locationClient;
-  LocationRequest locationRequest;
 
   int indexAnterior = 0;
 
@@ -60,13 +42,11 @@ implements
 
     bar.addTab(
         bar.newTab()
-        //.setText(getString(R.string.title_fragment_list)) // TODO: textos
         .setText("LISTADO")
         .setTabListener(this));
 
     bar.addTab(
         bar.newTab()
-        //.setText(getString(R.string.title_fragment_flags)) // TODO: textos
         .setText("MAPA")
         .setTabListener(this));
 
@@ -75,26 +55,6 @@ implements
     .add(R.id.mainContent, fragments[LISTADO])
     .add(R.id.mainContent, fragments[MAPA])
     .commit();
-
-    locationClient = new LocationClient(getActivity(), this, this);
-    locationRequest = LocationRequest.create();
-    
-    locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
-    locationRequest.setFastestInterval(FAST_INTERVAL_CEILING_IN_MILLISECONDS);
-    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-
-    FragmentManager manager = getActivity().getSupportFragmentManager();
-
-    if (serviciosConectados()) {
-      manager.beginTransaction().show(fragments[MAPA]).commit();
-    } else {
-      manager.beginTransaction().hide(fragments[MAPA]).commit();
-    }
   }
 
   @Override
@@ -104,20 +64,22 @@ implements
   }
 
   @Override
-  public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
-    // TODO Auto-generated method stub
+  public void onResume() {
+    super.onResume();
+    establecerVisibilidad();
+  }
 
+  @Override
+  public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
   }
 
   @Override
   public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
-    // TODO Auto-generated method stub
   }
 
   @Override
   public void onTabSelected(Tab tab, FragmentTransaction transaction) {
     setContent(tab.getPosition());
-
   }
 
   private void setContent(int index) {
@@ -138,31 +100,18 @@ implements
     indexAnterior = index;
   }
 
-  @Override
-  public void onConnectionFailed(ConnectionResult connectionResult) {
-    if (connectionResult.hasResolution()) {
-      try {
-        connectionResult.startResolutionForResult(
-            getActivity(),
-            CONNECTION_FAILURE_RESOLUTION_REQUEST);
-      } catch (IntentSender.SendIntentException e) {
-        Log.e("ERROR",Log.getStackTraceString(e));
-      }
+  private void establecerVisibilidad() {
+    FragmentManager manager = getActivity().getSupportFragmentManager();
+
+    if (googlePlayDisponible()) {
+      manager.beginTransaction().show(fragments[MAPA]).commit();
     } else {
-      Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-          connectionResult.getErrorCode(),
-          getActivity(),
-          CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-      if (errorDialog != null) {
-        ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-        errorFragment.setDialog(errorDialog);
-        errorFragment.show(getActivity().getSupportFragmentManager(), "dialog");
-      }
+      manager.beginTransaction().hide(fragments[MAPA]).commit();
     }
+    
   }
-
-  private boolean serviciosConectados() {
+  
+  private boolean googlePlayDisponible() {
     int resultCode =
         GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
 
@@ -179,44 +128,5 @@ implements
       }
       return false;
     }
-  }
-
-  @Override
-  public void onStart() {
-    super.onStart();
-    locationClient.connect();
-  }
-
-  @Override
-  public void onStop() {
-    super.onStop();
-    if (locationClient.isConnected()) {
-      locationClient.removeLocationUpdates(this);
-      locationClient.disconnect();
-    }
-  }
-
-  @Override
-  public void onConnected(Bundle connectionHint) {
-    actualizarPosicion(locationClient.getLastLocation());
-    locationClient.requestLocationUpdates(locationRequest, this);
-  }
-
-  @Override
-  public void onDisconnected() {
-  }
-  
-  @Override
-  public void onLocationChanged(Location location) {
-    actualizarPosicion(location);
-  }
-
-  public void actualizarPosicion(Location currentLocation ) {
-    String latLng = "No disponible";
-    if (currentLocation != null) {
-      latLng = "Lat:" + currentLocation.getLatitude() 
-        + " lon:"+ currentLocation.getLongitude();
-    }
-    Log.d("GPS", latLng);
   }
 }
